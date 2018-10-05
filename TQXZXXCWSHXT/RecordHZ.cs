aaabdcf11e4e42517b08e42e343fa3bf6f2480b5
zhaohuaihe100 <zhaohuaihe100@163.com>
2018-10-06 04:24:21 +0800

    增加mysql-connector v4.0后

--- TQXZXXCWSHXT/RecordHZ.cs
+++ TQXZXXCWSHXT/RecordHZ.cs
@@ -1,7 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Data.SqlClient; 
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+//using System.Data;
 
 namespace TQXZXXCWSHXT
 {
@@ -18,6 +28,7 @@ namespace TQXZXXCWSHXT
         private string zflK;  //报账总分类科目
         private string mxflK;  //报账明细分类科目
         //private string 
+        public static string sqlconn;
 
       private ZsJe[] zsje7; //张数和金额共有7种类型要记录，总金额0、总现金1、总转账2、合格现金3、
         //合格转账4、不合格现金5、不合格转账6，。
@@ -129,5 +140,99 @@ namespace TQXZXXCWSHXT
 
     }
 
-    //public class Connect
+    public class MysqlConnector
+{
+    string server = null;
+    string userid = null;
+    string password = null;
+    string database = null;
+    string port = "3306";
+    string charset = "utf-8";
+
+    public MysqlConnector() { }
+    public MysqlConnector SetServer(string server)
+    {
+        this.server = server;
+        return this;
+    }
+
+    public MysqlConnector SetUserID(string userid)
+    {
+        this.userid = userid;
+        return this;
+    }
+
+    public MysqlConnector SetDataBase(string database)
+    {
+        this.database = database;
+        return this;
+    }
+
+    public MysqlConnector SetPassword(string password)
+    {
+        this.password = password;
+        return this;
+    }
+    public MysqlConnector SetPort(string port)
+    {
+        this.port = port;
+        return this;
+    }
+    public MysqlConnector SetCharset(string charset)
+    {
+        this.charset = charset;
+        return this;
+    }
+
+
+
+    #region  建立MySql数据库连接
+    /// <summary>
+    /// 建立数据库连接.
+    /// </summary>
+    /// <returns>返回MySqlConnection对象</returns>
+    private MySqlConnection GetMysqlConnection()
+    {
+        string M_str_sqlcon = string.Format("server={0};user id={1};password={2};database={3};port={4};Charset={5}", server, userid, password, database, port, charset);
+        MySqlConnection myCon = new MySqlConnection(M_str_sqlcon);
+        return myCon;
+    }
+    #endregion
+
+    #region  执行MySqlCommand命令
+    /// <summary>
+    /// 执行MySqlCommand
+    /// </summary>
+    /// <param name="M_str_sqlstr">SQL语句</param>
+    public void ExeUpdate(string M_str_sqlstr)
+    {
+        MySqlConnection mysqlcon = this.GetMysqlConnection();
+        mysqlcon.Open();
+        MySqlCommand mysqlcom = new MySqlCommand(M_str_sqlstr, mysqlcon);
+        mysqlcom.ExecuteNonQuery();
+        mysqlcom.Dispose();
+        mysqlcon.Close();
+        mysqlcon.Dispose();
+    }
+    #endregion
+
+    #region  创建MySqlDataReader对象
+    /// <summary>
+    /// 创建一个MySqlDataReader对象
+    /// </summary>
+    /// <param name="M_str_sqlstr">SQL语句</param>
+    /// <returns>返回MySqlDataReader对象</returns>
+    public MySqlDataReader ExeQuery(string M_str_sqlstr)
+    {
+        Console.WriteLine(M_str_sqlstr);
+        MySqlConnection mysqlcon = this.GetMysqlConnection();
+        MySqlCommand mysqlcom = new MySqlCommand(M_str_sqlstr, mysqlcon);
+        mysqlcon.Open();
+        MySqlDataReader mysqlread = mysqlcom.ExecuteReader(CommandBehavior.CloseConnection);
+        return mysqlread;
+    }
+    #endregion
+}
+
+
 }
